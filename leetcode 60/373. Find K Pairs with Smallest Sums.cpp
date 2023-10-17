d217061a512dc189d3f4ea72a0cfddf23e870061
Sourav Majumder <souravmajumderk99@gmail.com>
2023-10-18 03:40:16 +0600

    373. Find K Pairs with Smallest Sum

---
+++ leetcode 60/373. Find K Pairs with Smallest Sums.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    vector<vector<int>> kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k)
+    {
+        vector<vector<int>> result;
+        if (nums1.empty() || nums2.empty() || k <= 0)
+            return result;
+
+        auto comp = [&](const pair<int, int> &a, const pair<int, int> &b)
+        {
+            return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+
+        for (int i = 0; i < nums1.size() && i < k; i++)
+        {
+            pq.emplace(i, 0);
+        }
+
+        while (k-- > 0 && !pq.empty())
+        {
+            auto curr = pq.top();
+            pq.pop();
+            result.push_back({nums1[curr.first], nums2[curr.second]});
+            if (curr.second + 1 < nums2.size())
+            {
+                pq.emplace(curr.first, curr.second + 1);
+            }
+        }
+
+        return result;
+    }
+};
