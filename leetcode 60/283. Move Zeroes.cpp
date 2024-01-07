ed98d214d6b491abc02920d9c7e0aa039dfd3799
Sourav Majumder <souravmajumderk99@gmail.com>
2024-01-07 14:30:14 +0600

    283. Move Zeroes

---
+++ leetcode 60/283. Move Zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = 0, j = 0;
+        while (i < n)
+        {
+            if (nums[i] != 0)
+            {
+                nums[j++] = nums[i];
+            }
+            i++;
+        }
+        while (j < n)
+        {
+            nums[j++] = 0;
+        }
+    }
+};
